c5692127ceb5bea44a792b86a1230b4639f3438b
sourness1979@gmail.com <Fedor15*21>
2023-03-12 21:57:54 +0300

    AdminFilter но без проверки

--- web/src/main/java/by/karnilovich/web/servlet/AdminFilter.java
+++ web/src/main/java/by/karnilovich/web/servlet/AdminFilter.java
@@ -1,5 +1,6 @@
 package by.karnilovich.web.servlet;
 
+import by.karnilovich.service.person.PersonService;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +10,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import static by.karnilovich.web.servlet.AuthFilter.LOGIN_JSP;
+import static by.karnilovich.web.servlet.LoginServlet.LOGGED_IN_USER;
 
 @WebFilter(filterName = "AdminFilter", urlPatterns = "/admin")
 public class AdminFilter implements Filter {
@@ -26,5 +31,14 @@ public class AdminFilter implements Filter {
         String requestURI = req.getRequestURI();
 
         String requestURL = requestURI.replaceFirst(req.getContextPath(), "");
+
+        if ((Objects.nonNull(session)) && PersonService.findByEmail((String)req.getSession().getAttribute("email")).getRole().equals("ROLE_ADMIN")) {
+            LOGGER.debug("Authentication with ROLE_ADMIN " + requestURI);
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            LOGGER.debug("User is not authorized. Redirect to login page " + requestURI);
+            req.getRequestDispatcher(LOGIN_JSP)
+                    .forward(servletRequest, servletResponse);
+        }
     }
 }
